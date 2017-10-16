51dfb34a498e90a6739fcdb873728bba97fdbe07
Victor Atanasov <viktor_atanasovv@yahoo.com>
2017-10-16 15:55:32 +0300

    mongoDB connection to mLab added, small UI changes

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const port = 3001;
-const connectionString = 'mongodb://localhost/posts-db';
+const connectionString = 'mongodb://admin:admin@ds121565.mlab.com:21565/nodejstraveller';
 const sessionSecret = 'Purple Unicorn';
 
 module.exports = { port, connectionString, sessionSecret }
